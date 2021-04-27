aa591f77200d370a16b9e5b5ed46075e58d36b87
Sami0412 <samiw0412@gmail.com>
2021-04-27 12:24:47 +0100

    ProfileController created

--- freeCodeGram/resources/views/home.blade.php
+++ freeCodeGram/resources/views/home.blade.php
@@ -7,7 +7,7 @@
             <img src="https://scontent-man2-1.cdninstagram.com/v/t51.2885-19/s150x150/97566921_2973768799380412_5562195854791540736_n.jpg?tp=1&_nc_ht=scontent-man2-1.cdninstagram.com&_nc_ohc=o20BuuJm14gAX9UTHYZ&edm=ABfd0MgAAAAA&ccb=7-4&oh=c62f53edf365f892f76ac2de421d6a10&oe=60A60467&_nc_sid=7bff83" class="rounded-circle" alt="">
         </div>
         <div class="col-9 pt-5">
-            <div><h1>freeCodeCamp</h1></div>
+            <div><h1>{{ $user->username }}</h1></div>
             <div class="d-flex">
                 <div class="pr-5"><strong>153</strong> posts</div>
                 <div class="pr-5"><strong>23k</strong> followers</div>
