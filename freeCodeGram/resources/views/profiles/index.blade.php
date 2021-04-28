a654bfe463113e42493b3414e73efdc77873914d
Sami0412 <samiw0412@gmail.com>
2021-04-28 12:59:59 +0100

    Basic post create form added

--- freeCodeGram/resources/views/profiles/index.blade.php
+++ freeCodeGram/resources/views/profiles/index.blade.php
@@ -7,15 +7,18 @@
             <img src="https://scontent-man2-1.cdninstagram.com/v/t51.2885-19/s150x150/97566921_2973768799380412_5562195854791540736_n.jpg?tp=1&_nc_ht=scontent-man2-1.cdninstagram.com&_nc_ohc=o20BuuJm14gAX9UTHYZ&edm=ABfd0MgAAAAA&ccb=7-4&oh=c62f53edf365f892f76ac2de421d6a10&oe=60A60467&_nc_sid=7bff83" class="rounded-circle" alt="">
         </div>
         <div class="col-9 pt-5">
-            <div><h1>{{ $user->username }}</h1></div>
+            <div class="d-flex justify-content-between align-items-baseline">
+                <h1>{{ $user->username }}</h1>
+                <a href="#">Add New Post</a>
+            </div>
             <div class="d-flex">
                 <div class="pr-5"><strong>153</strong> posts</div>
                 <div class="pr-5"><strong>23k</strong> followers</div>
                 <div class="pr-5"><strong>212</strong> following</div>
             </div>
-            <div class="pt-4 font-weight-bold">freecodecamp.org</div>
-            <div>We're a global community of people learning to code together. We're an open source, donor-supported, 501(c)(3) nonprofit.</div>
-            <div><a href="~">www.freecodecamp.org</a></div>
+            <div class="pt-4 font-weight-bold">{{ $user->profile->title }}</div>
+            <div>{{ $user->profile->description }}</div>
+            <div><a href="#">{{ $user->profile->url }}</a></div>
         </div>
     </div>
     <div class="row pt-5">
--- freeCodeGram/routes/web.php
+++ freeCodeGram/routes/web.php
@@ -19,4 +19,6 @@ Route::get('/', function () {
 
 Auth::routes();
 
+Route::get('/p/create', [App\Http\Controllers\PostsController::class, 'create']);  //Must use namespace in Laravel 8 - don't follow tutorial!
+
 Route::get('/profile/{user}', [App\Http\Controllers\ProfilesController::class, 'index'])->name('profile.show');
