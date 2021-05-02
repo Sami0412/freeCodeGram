cbf7cc4681a5ef179bc49c9b2bb1c7b060f608e9
Sami0412 <samiw0412@gmail.com>
2021-05-02 17:04:54 +0100

    Form validation added

--- freeCodeGram/routes/web.php
+++ freeCodeGram/routes/web.php
@@ -20,5 +20,6 @@ Route::get('/', function () {
 Auth::routes();
 
 Route::get('/p/create', [App\Http\Controllers\PostsController::class, 'create']);  //Must use namespace in Laravel 8 - don't follow tutorial!
+Route::post('/p', [App\Http\Controllers\PostsController::class, 'store']);
 
 Route::get('/profile/{user}', [App\Http\Controllers\ProfilesController::class, 'index'])->name('profile.show');
