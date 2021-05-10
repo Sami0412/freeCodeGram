beeb40c9e847338dac43b1ce0fc30a2a4a7bc1f8
Sami0412 <samiw0412@gmail.com>
2021-05-10 21:22:05 +0100

    Pagination added

--- freeCodeGram/resources/views/posts/index.blade.php
+++ freeCodeGram/resources/views/posts/index.blade.php
@@ -4,12 +4,14 @@
 <div class="container">
     @foreach($posts as $post)
     <div class="row">
-        <div class="col-8 offset-2">
-            <img src="/storage/{{ $post->image }}" class="w-100">
+        <div class="col-6 offset-3">
+            <a href="/profile/{{ $post->user->id }}">
+                <img src="/storage/{{ $post->image }}" class="w-100">
+            </a>
         </div>
     </div>
     <div class="row pt-2 pb-4">
-        <div class="col-8 offset-2">
+        <div class="col-6 offset-3">
             <div>
                 <p>
                     <span class="font-weight-bold">
@@ -22,6 +24,12 @@
         </div>
     </div>
     @endforeach
+
+    <div class="row">
+        <div class="col-12 d-flex justify-content-center">
+            {{ $posts->links('pagination::bootstrap-4') }}
+        </div>
+    </div>
 </div>
 
 @endsection
